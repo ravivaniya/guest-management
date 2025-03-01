2a71c4efae26700496592b443cc2490493519c6e
ravivaniya <ravivaniya4911@gmail.com>
2025-03-01 15:28:36 +0530

    analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -29,6 +31,8 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <Analytics />
+      <SpeedInsights />
     </html>
   );
 }
